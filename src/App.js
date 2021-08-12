c8aa21348bb54110a3415a2bda538b6c67e02451
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-08-12 19:36:21 +0800

    Cart Page

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ import { Route, Link } from "react-router-dom";
 import { Navbar, Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState } from "react";
-import { product } from "prelude-ls";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function App() {
-	const cartProducts = [];
-	const [cartItems, SetCartItems] = useState(cartProducts);
+	let cartProducts = [];
 	const init = () => {
 		const storage = localStorage.getItem("cartItems");
 		if (storage) cartProducts = JSON.parse(storage);
@@ -19,9 +17,11 @@ function App() {
 		cartProducts = localStorage.getItem("cartItems");
 	};
 
+	init();
+	const [cartItems, SetCartItems] = useState(cartProducts);
+
 	const updateItemsToCart = (newproduct) => {
 		if (cartProducts.find((product) => product.id == newproduct.id)) {
-			console.log(cartProducts);
 			const product_obj = cartProducts.find(
 				(product) => product.id == newproduct.id
 			);
@@ -30,26 +30,44 @@ function App() {
 			cartProducts.push(newproduct);
 		}
 		localStorage.setItem("cartItems", JSON.stringify(cartProducts));
+		SetCartItems(cartProducts);
+		console.log(cartItems);
 	};
 
 	const addToCartHandler = (product) => {
 		console.log("On App.js");
 		updateItemsToCart(product);
 	};
+	const editCartHandler = (product) => {
+		console.log("On App.js");
+		const product_obj = cartProducts.find(
+			(cart_product) => cart_product.id == product.id
+		);
+		product_obj.Qty = product.Qty;
+		localStorage.setItem("cartItems", JSON.stringify(cartProducts));
+		SetCartItems(cartProducts);
+	};
 	return (
 		<div className="App">
 			<Navbar expand="lg" variant="light" bg="light">
 				<Container>
-					<Navbar.Brand href="#">
+					<Navbar.Brand href="/">
 						<img src={logo} className="app-logo" />
 					</Navbar.Brand>
+					<div>
+						<Link className="link" to="/cart">
+							<i className="fa fa-cart-plus" aria-hidden="true"></i>
+						</Link>
+					</div>
 				</Container>
 			</Navbar>
 
 			<Route exact path="/" component={Products}>
 				<Products onAppAddToCart={addToCartHandler} />
 			</Route>
-			<Route exact path="/cart" component={Cart}></Route>
+			<Route exact path="/cart" component={Cart}>
+				<Cart onAppEditCart={editCartHandler} cartItems={cartItems}></Cart>
+			</Route>
 		</div>
 	);
 }

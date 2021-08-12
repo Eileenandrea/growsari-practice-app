5bfbc87df40d071348858d88081b17ed5e160824
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-08-12 12:10:26 +0800

    creating product and cart route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Cart from "./Cart/Cart";
+import Products from "./Product/Products";
+import { Route, Link } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Route exact path="/" component={Products}></Route>
+			<Route exact path="/cart" component={Cart}></Route>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Product/Products.js
@@ -0,0 +1,9 @@
+function Products() {
+	return (
+		<div>
+			<h1>Product Lists</h1>
+		</div>
+	);
+}
+
+export default Products;

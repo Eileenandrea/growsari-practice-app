5bd954757b9cb02d338895751259b1d64edb5a32
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-08-12 23:17:09 +0800

    add Pagination

--- src/Product/Products.js
+++ src/Product/Products.js
@@ -1,22 +1,56 @@
 import { Product_data_list } from "./productDataList";
 import { Container, Button } from "react-bootstrap";
 import ProductItem from "./ProductItem";
+import ReactPaginate from "react-paginate";
+import { useState } from "react";
+import "./Product.scss";
+
 function Products(props) {
-	const product_data_lists = Product_data_list;
+	const [productList, setProductList] = useState(
+		Product_data_list.slice(0, 50)
+	);
+
+	const [pageNumber, setPageNumber] = useState(0);
+
+	const productPerPage = 12;
+	const pageVisited = pageNumber * productPerPage;
+
+	const displayProduct = productList.slice(
+		pageVisited,
+		pageVisited + productPerPage
+	);
+
 	const addToCartHandler = (product) => {
 		props.onAppAddToCart(product);
+	};
+	const pageCount = Math.ceil(Product_data_list.length / productPerPage);
+	const changePage = ({ selected }) => {
+		setPageNumber(selected);
 	};
 	return (
 		<Container>
 			<div className="row">
 				<h1>Product Lists</h1>
-				{product_data_lists.map((item) => (
+				{displayProduct.map((item) => (
 					<ProductItem
 						onAddToCart={addToCartHandler}
 						data={item}
 						key={item.id}
 					/>
 				))}
+			</div>
+			<div className="mt-5">
+				<ReactPaginate
+					previousLabel={"Previous"}
+					nextLabel={"Next"}
+					pageCount={pageCount}
+					onPageChange={changePage}
+					containerClassName={"paginationBttns"}
+					previousLinkClassName={"previousBttn"}
+					nextLinkClassName={"nextBttb"}
+					disabledClassName={"paginationDisabled"}
+					activeClassName={"paginationActive"}
+				/>
 			</div>
 		</Container>
 	);
